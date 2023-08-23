5bcb17f7628cb1ac2c654a474c304dd1faca5f7e
josephanderson3 <80002362+joseph.anderson.3@ndsu.edu>
2023-08-22 19:20:33 -0500

    Fixed two links being alligned incorrectly on the page.

--- nytfront/src/Components/ShowByID/ShowByID.js
+++ nytfront/src/Components/ShowByID/ShowByID.js
@@ -5,6 +5,8 @@ import { Container } from "@mui/material";
 import Rating from '@mui/material/Rating';
 import APIURLS from "../../Constants/APIURLs";
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
 import {
     Link as RRLink, // the RRLink is because material UI also uses link this clears up any confusion.
      } from "react-router-dom";
@@ -46,8 +48,10 @@ function ShowByID(props){
           <Rating name="half-rating" defaultValue={0} precision={0.5}   size="large" value={ratingValue} readOnly/>
 
         </Container>
-        <Button size="small" component={RRLink}   to={'/updatereview/'} state={bookData} fromShowByID={fromSearchReviewByTitle}>Update Review and Rating</Button>
-        <Button size="small" component={RRLink}   to={'/deletereview/'} state={bookData} fromShowByID={fromSearchReviewByTitle}>Delete Review</Button>
+        <Box   display="flex" justifyContent="center" alignItems="center" sx={{ p: 2 }}>
+          <Button size="small" component={RRLink}   to={'/updatereview/'} state={bookData} fromShowByID={fromSearchReviewByTitle}>Update Review and Rating</Button>
+          <Button size="small" component={RRLink}   to={'/deletereview/'} state={bookData} fromShowByID={fromSearchReviewByTitle}>Delete Review</Button>   
+        </Box>
            </> 
     )
 
